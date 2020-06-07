0b36f904733c5e574caf49186d34844326897dd5
Tutor Exilius <tutorexilius@gmail.com>
2020-06-07 21:22:25 +0200

    Replaced compositor deactivation by using kwriteconfig5

--- deactivate_compositor.sh
+++ deactivate_compositor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-config_file="$(find ~ -iname kwinrc)"
-sed -i 's/^Enabled=true$/Enabled=false/g' "$config_file"
+#config_file="$(find ~ -iname kwinrc)"
+#sed -i 's/^Enabled=true$/Enabled=false/g' "$config_file"
+kwriteconfig5 --file kwinrc --group Compositing --key Enabled false
